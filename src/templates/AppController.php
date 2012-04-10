9cd3cc129b90634cb1345dde503ed79a73af7b2a
zach <zach@0c957f68-fb5b-42df-94e7-f001b4a06e9d>
2012-04-10 19:23:37 +0000

    Adding the AppController template
    git-svn-id: http://svn.vectorw3b.com/vectorized-php/trunk@12 0c957f68-fb5b-42df-94e7-f001b4a06e9d

---
+++ src/templates/AppController.php
@@ -0,0 +1,16 @@
+<?php 
+return <<<EOF
+<?php
+namespace {$namespace}\Controllers;
+
+\Vzed\import('vzed.controller');
+
+use \Vzed\Controller;
+
+class Application extends Controller {
+
+}
+
+?>
+EOF;
+?>
