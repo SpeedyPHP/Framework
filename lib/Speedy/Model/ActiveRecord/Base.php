fe9bd94f2b35d4d391ab732d49f7ddf612c3d78c
Zachary Quintana <zach.quintana@gmail.com>
2012-06-13 13:46:26 -0700

    Adding mixins for ActiveRecord model

--- lib/Speedy/Model/ActiveRecord/Base.php
+++ lib/Speedy/Model/ActiveRecord/Base.php
@@ -3,5 +3,139 @@ namespace Speedy\Model\ActiveRecord;
 
 class Base extends \Speedy\ActiveRecord\Model {
 
+	/**
+	* Method mixins
+	* @var array of objects
+	*/
+	protected $_mixins = array();
+	
+	/**
+	 * Mixin objects
+	 * @var array of mixin instances
+	 */
+	protected $_mixinObjs = array();
+	
+	/**
+	 * $var boolean
+	 */
+	protected $_mixinsLoaded = false;
+	
+	
+	
+	public function __construct(array $attributes=array(), $guard_attributes=true, $instantiating_via_find=false, $new_record=true) {
+		parent::__construct($attributes, $guard_attributes, $instantiating_via_find, $new_record);
+		
+		
+	}
+	
+	/**
+	 * Checks if mixins already loaded
+	 * @return boolean
+	 */
+	protected function _loadedMixins() {
+		return $this->_mixinsLoaded;
+	}
+	
+	/**
+	 * Checks if mixin is already loaded
+	 * @return boolean
+	 */
+	protected function _hasMixin($mixin) {
+		return isset($this->_mixinObjs[$mixin]);
+	}
+	
+	/**
+	 * loads mixins
+	 * @return $this
+	 */
+	protected function _loadMixins() {
+		if ($this->_loadedMixins()) return $this;
+		if ($this->respondsTo('__loadMixins')) {
+			$this->_mixins	= array_merge($this->_mixins, (array) $this->__loadMixins());
+		}
+	
+	
+		foreach ($this->_mixins as $mixin => $options) {
+			if (is_int($mixin)) {
+				$mixin = $options;
+			}
+				
+			if (isset($this->_mixinObjs[$mixin])) {
+				continue;
+			}
+				
+			if (is_string($mixin) && class_exists($mixin)) {
+				$class = $mixin;
+			} elseif (is_string($mixin)) {
+				import($mixin);
+				$class	= \Speedy\Loader::toClass($mixin);
+			}
+	
+			if (!$class) {
+				continue;
+			}
+				
+			$instance	= new $class((is_array($options) ? $options : null));
+				
+			$this->_mixinObjs[$mixin] = $instance;
+		}
+	
+		$this->_mixinsLoaded = true;
+		return $this;
+	}
+	
+	/**
+	 * Attempts to call a mixin
+	 *
+	 */
+	protected function _callMixin($name, $args) {
+		foreach ($this->_getMixins() as $instance) {
+			if ($instance instanceof \Speedy\Object && $instance->respondsTo($name)) {
+				return call_user_func_array(array($instance, $name), $args);
+			}
+		}
+	
+		return parent::__call($name, $args);
+	}
+	
+	/**
+	 * Gets a mixin
+	 * @return mixin instance
+	 */
+	private function _getMixin($mixin) {
+		if (empty($this->_mixinObjs) && !$this->_loadedMixins()) {
+			$this->_loadMixins();
+		}
+		
+		return ($this->_hasMixin($mixin)) ? $this->_mixinObjs[$mixin] : null;
+	}
+	
+	/**
+	 * Gets all mixins
+	 * @return array of mixin instances
+	 */
+	private function _getMixins() {
+		if (empty($this->_mixinObjs) && !$this->_loadedMixins()) {
+			$this->_loadMixins();
+		}
+	
+		return $this->_mixinObjs;
+	}
+	
+	public function respondsTo($method) {
+		return method_exists($this, $method);
+	}
+	
+	/**
+	 * Magic methods for magic getters, setters, and methods
+	 */
+	public function __call($name, $args) {
+		if (!$this->_loadedMixins()) {
+			$this->_loadMixins();
+		}
+		
+		return $this->_callMixin($name, $args);
+	}
+	
 }
 ?>
