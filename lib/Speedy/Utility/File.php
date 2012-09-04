dd0dc2edf492c180dc20e29252746f3313d91695
Zachary Quintana <zach.quintana@gmail.com>
2012-09-04 14:54:22 -0700

    Adding file utils class

---
+++ lib/Speedy/Utility/File.php
@@ -0,0 +1,42 @@
+<?php
+namespace Speedy\Utility;
+
+
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
+class File {
+
+	
+	public static function cp($source, $dest, $resouce = null) {
+		return copy($source, $dest, $resource);
+	}
+	
+	public static function cp_r($source, $dest, $resouce = null) {
+		$dir = opendir($source);
+		@mkdir($dest);
+		while(false !== ($file = readdir($dir))) {
+			if (($file != '.') && ($file != '..')) {
+				if (is_dir($source . DS . $file)) {
+					self::cp_r($source . DS . $file, $dest . DS . $file, $resource);
+				}
+				else {
+					self::cp($source . DS . $file, $dest . DS . $file, $resource);
+				}
+			}
+		}
+		closedir($dir);
+	}
+	
+	public static function rm_rf($dirname, $context = null) {
+		foreach(glob($dirname . DS . '*') as $file) {
+			if(is_dir($file))
+				self::rm_rf($file, $context);
+			else
+				unlink($file, $context);
+		}
+		
+		return rmdir($dirname, $context);
+	}
+	
+}
+?>
