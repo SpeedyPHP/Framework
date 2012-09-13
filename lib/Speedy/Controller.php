53cd846c6a93f2f84bbb319e7a51bcbcd55e1230
Zachary Quintana <zach.quintana@gmail.com>
2012-09-12 17:42:46 -0700

    Update

--- lib/Speedy/Controller.php
+++ lib/Speedy/Controller.php
@@ -320,7 +320,7 @@ class Controller extends Object {
 	 */
 	protected function render() {
 		$args	= func_get_args();
-		$path	= (isset($args[0]) && is_string($args[0])) ? array_shift($args) : null;
+		$path	= array_shift($args);
 		$options= (isset($args[0]) && is_array($args[0])) ? array_shift($args) : array();
 		
 		if ($this->isRendered()) return;
