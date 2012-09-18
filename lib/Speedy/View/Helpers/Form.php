471c10f65a77a9064f6b077e13609fed8dae0a57
Zachary Quintana <zach.quintana@gmail.com>
2012-09-18 11:14:04 -0700

    Fix

--- lib/Speedy/View/Helpers/Form.php
+++ lib/Speedy/View/Helpers/Form.php
@@ -184,7 +184,7 @@ class Form extends Object {
 		$options = [];
 		if (!empty($collection)) {
 			foreach ($collection as $record) {
-				$options[$record->{$key}] = [$record->{$value}];
+				$options[$record->{$key}] = $record->{$value};
 			}
 		}
 		
