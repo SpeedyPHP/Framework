836f287ee52bb533ddd6ba5c9143ef8774e2b907
Zachary Quintana <zach.quintana@gmail.com>
2012-09-18 11:59:59 -0700

    Fix

--- lib/Speedy/Model/ActiveRecord/Base.php
+++ lib/Speedy/Model/ActiveRecord/Base.php
@@ -5,8 +5,10 @@ class Base extends \Speedy\ActiveRecord\Model {
 	
 	public function __construct(array $attributes=array(), $guard_attributes=true, $instantiating_via_find=false, $new_record=true) {
 		parent::__construct($attributes, $guard_attributes, $instantiating_via_find, $new_record);
-			
+		$this->_construct();
 	}
+	
+	public function _construct() {}
 	
 }
 ?>
