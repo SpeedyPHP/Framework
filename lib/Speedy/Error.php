37e337566f7986badeeff866f5c0390cbe34026d
Zachary Quintana <zach.quintana@gmail.com>
2013-07-17 15:01:12 -0700

    Adding the new Error class

---
+++ lib/Speedy/Error.php
@@ -0,0 +1,112 @@
+<?php
+namespace Speedy;
+
+
+use \App as AppInUse;
+use Speedy\Http\Exception as HttpException;
+use Speedy\View;
+use Speedy\Request;
+
+class Error {
+
+	/**
+	 * Exception error caught
+	 * @var Exception
+	 */
+	protected $error;
+
+
+	protected $report = false;
+
+
+	/**
+	 * Error handler for Speedy Apps
+	 * @param $e Exception
+	 * @return $this Speedy\Error
+	 */
+	public function __construct(\Exception $e) {
+		$this->handleError($e);
+		$this->report = AppInUse::instance()->config('errors.report');
+
+		return $this;
+	}
+
+	private function shouldReport() {
+		return $this->report;
+	}
+
+	/**
+	 * Handle error entry point
+	 * @param $e Exception
+	 * @return null
+	 */
+	protected function handleError($e) {
+		$this->setError($e);
+
+		$className = str_replace('\\', '', get_class($e));
+		if ($e instanceof HttpException) {
+			return $this->handleHttpException();
+		}
+
+		http_response_code(500);
+	}
+
+	protected function handleHttpException() {
+		$code = $this->error()->getCode();
+		http_response_code($code);
+
+		$viewFile = "errors" . DS . $code;
+		if (!View::instance()->findFile($viewFile)) {
+			return $this->default404(); // Default view
+		}
+
+		echo View::instance()->render($viewFile);
+	}
+
+	protected function default404() {
+		$html = [
+			'<html>',
+			'<head>',
+			'</head>',
+			'<body>',
+			'<div class="page-header">',
+			'<h1>404</h1>',
+			'</div>',
+			'<div class="description"><p>%s</p></div>',
+			'<div class="params">',
+			'<h2>Request Parameters</h2>',
+			'<pre>%s</pre>',
+			'</div>',
+			'</body>',
+			'</html>'
+			];
+			$html = implode("\n", $html);
+		echo sprintf($html, 
+					$this->error()->getMessage(), 
+					print_r(Request::instance()->params(), true));
+		return;
+	}
+
+	/**
+	 * Setter for error
+	 * @param $e Exception
+	 * @return $this Speedy\Error
+	 */
+	protected function setError($e) {
+		$this->error = $e;
+		return $this;
+	}
+
+	/**
+	 * Getter for error
+	 * @return $this->error Exception
+	 */
+	public function error() {
+		return $this->error;
+	}
+
+	public function handleException() {
+
+	}
+
+}
